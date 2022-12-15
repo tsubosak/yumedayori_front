a801520242b86c9bcc2458a0c8af5cc471f3d4cd
tsubosak <koseit+git@googlegroups.com>
2022-12-15 21:47:40 +0900

    :speech_balloon: update label about seiyu

--- src/constants.ts
+++ src/constants.ts
@@ -2,12 +2,12 @@ export const API_ENDPOINT = "http://localhost:7000"
 
 export const PARENT_TYPE_JA_BY_CHILD = {
   CONSIST_OF: "メンバー",
-  VOICED_BY: "ボーカル",
+  VOICED_BY: "声優",
 }
 
 export const PARENT_TYPE_JA_BY_PARENT = {
   CONSIST_OF: "所属",
-  VOICED_BY: "ボーカル",
+  VOICED_BY: "声優担当",
 }
 
 export const CREDITED_AS_JA = {
